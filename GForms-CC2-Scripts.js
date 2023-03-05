1c5c4aff59e351b4f84f5f64eca7ebe37fec38ec
Dan <dmalengela@yahoo.com>
2023-03-05 23:18:44 +0200

    Added show/hide script to CC2 Actions field

--- GForms-CC2-Scripts.js
+++ GForms-CC2-Scripts.js
@@ -1,16 +1,42 @@
 /////////////////////////////////////// Ratings Color Change ///////////////////////////////////////////////////
 if ('{controlvalue}'.includes('Good')) { 
-    $('{this}').css('background-color', 'green').css('color', 'black'); 
+    $('{this}').css('background-color', 'green').css('color', 'black');
+    $('#FORM_CC2_AsBuiltActions').parent().hide(); 
 } 
 else if ('{controlvalue}'.includes('Moderate')) { 
     $('{this}').css('background-color', 'yellow').css('color', 'black'); 
+    $('#FORM_CC2_AsBuiltActions').parent().show();
 } 
 else if ('{controlvalue}'.includes('Weak')) { 
     $('{this}').css('background-color', 'orange').css('color', 'white');
+    $('#FORM_CC2_AsBuiltActions').parent().show();
 } 
 else if ('{controlvalue}'.includes('Deficient')) { 
     $('{this}').css('background-color', 'red').css('color', 'white'); 
+    $('#FORM_CC2_AsBuiltActions').parent().show();
 }
 else { 
     $('{this}').css('background-color', 'white').css('color', 'Black'); 
+    $('#FORM_CC2_AsBuiltActions').parent().hide();
 };
+
+
+//////////////////////// FOR AFTER BROWSER LOAD /////////////////////////
+setTimeout(function () {
+    if ($('#FORM_CC1.AsbuiltCtrlRating').val('Good')) { 
+        $('#FORM_CC2_AsBuiltActions').parent().hide();
+    } 
+    else if ($('#FORM_CC1.AsbuiltCtrlRating').val('Moderate')) { 
+        $('#FORM_CC2_AsBuiltActions').parent().show(); 
+    } 
+    else if ($('#FORM_CC1.AsbuiltCtrlRating').val('Weak')) { 
+        $('#FORM_CC2_AsBuiltActions').parent().show(); 
+    } 
+    else if ($('#FORM_CC1.AsbuiltCtrlRating').val('Deficient')) { 
+        $('#FORM_CC2_AsBuiltActions').parent().show();  
+    } 
+    else if ($('#FORM_CC1.AsbuiltCtrlRating').val('[NULL]')){
+        $('#FORM_CC2_AsBuiltActions').parent().hide(); 
+    };
+    
+}, 1500);
