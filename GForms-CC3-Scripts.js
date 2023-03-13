af2113873ab747d69081319cb36e6c6d99c13a5c
Dan <dmalengela@yahoo.com>
2023-03-13 10:44:39 +0200

    Fixed the Rating Filter script for CC3 Form

--- GForms-CC3-Scripts.js
+++ GForms-CC3-Scripts.js
@@ -40,3 +40,84 @@ setTimeout(function () {
     };
     
 }, 1500);
+
+
+
+//////////////////////////////////// Ratings Value Filter & Change ////////////////////////////////////////////
+setTimeout(function () {
+    if ($('#FORM_CC3.SGWaterCtrlRating').val('Good')) { 
+        $('#FORM_CC3_SGWaterActions').parent().hide();
+    } 
+    else if ($('#FORM_CC3.SGWaterCtrlRating').val('Moderate')) { 
+        $('#FORM_CC3_SGWaterActions').parent().show(); 
+    } 
+    else if ($('#FORM_CC3.SGWaterCtrlRating').val('Weak')) { 
+        $('#FORM_CC2_SGWaterActions').parent().show();
+    } 
+    else if ($('#FORM_CC3.SGWaterCtrlRating').val('Deficient')) { 
+        $('#FORM_CC3_SGWaterActions').parent().show(); 
+    } 
+    else if ($('#FORM_CC3.SGWaterCtrlRating').val('[NULL]')){
+        $('#FORM_CC3_SGWaterActions').parent().hide(); 
+    };
+    
+}, 1500);
+
+
+
+ setTimeout(function() {
+    let adequacySelect = document.querySelector('#FORM_CC3_SGWaterDesignAdequacy');
+    let adequacyValue = adequacySelect.options[adequacySelect.selectedIndex].value;
+    let effectiveSelect = document.querySelector('#FORM_CC3_SGWaterOperatingEffect');
+    let effectiveValue = effectiveSelect.options[effectiveSelect.selectedIndex].value;
+    let ratingOptions = document.querySelectorAll('#FORM_CC3_SGWaterCtrlRating option');
+    for (let i = 0; i < ratingOptions.length; i++) {
+       let ratingOption = ratingOptions[i];
+       if (adequacyValue === 'Adequate' && effectiveValue === 'Effective') {
+          if (ratingOption.value === 'Good') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else if (adequacyValue === 'Partially Adequate' && effectiveValue === 'Effective') {
+          if (ratingOption.value === 'Moderate' || ratingOption.value === 'Weak') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else if (adequacyValue === 'Partially Adequate' && effectiveValue === 'Partially effective') {
+          if (ratingOption.value === 'Moderate' || ratingOption.value === 'Weak') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else if (adequacyValue === 'Partially Adequate' && effectiveValue === 'Partially effective') {
+          if (ratingOption.value === 'Moderate' || ratingOption.value === 'Weak') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else if (adequacyValue === 'Partially Adequate' && effectiveValue === 'Ineffective') {
+          if (ratingOption.value === 'Weak' || ratingOption.value === 'Deficient') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else if (adequacyValue === 'Adequate' && effectiveValue === 'Ineffective') {
+          if (ratingOption.value === 'Weak' || ratingOption.value === 'Deficient') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else if (adequacyValue === 'Inadequate') {
+          if (ratingOption.value === 'Deficient') {
+             ratingOption.style.display = 'initial';
+          } else {
+             ratingOption.style.display = 'none';
+          }
+       } else {
+          ratingOption.style.display = 'initial';
+       }
+    }
+ },
+ 200);
